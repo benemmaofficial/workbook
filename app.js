3c675a0d052b4c86db932b3dd3b552699360df36
Ben Emma <40636114+benemmaofficial@users.noreply.github.com>
2019-09-29 21:11:35 +0100

    Add files via upload
    Plain Vanilla javascript web app demo (Unit 15 tasks)

---
+++ app.js
@@ -0,0 +1,57 @@
+//VALIDATE WORKBOOK APPLICATION FORM
+function validate() {
+  if (document.form.name.value == "") {
+      displayAlert.create ({
+          message: 'Please enter your full name',
+          header: 'Required!',
+          buttons: ['OKAY'],
+      }).then(alertElement => {
+          alertElement.present();
+      })
+      document.form.name.focus();
+      return false;
+  }
+  if (document.form.email.value == "") {
+      displayAlert.create ({
+          message: 'Please enter an email address...',
+          header: 'Required!',
+          buttons: ['OKAY'],
+      }).then(alertElement => {
+          alertElement.present();
+      })
+      document.form.email.focus();
+      return false;
+  }
+  if (document.form.postcode.value == "") {
+      displayAlert.create ({
+          message: 'Please enter your postcode',
+          header: 'Required!',
+          buttons: ['OKAY'],
+      }).then(alertElement => {
+          alertElement.present();
+      })
+      document.form.postcode.focus();
+      return false;
+  }
+  if (document.form.choice.value == "") {
+      displayAlert.create ({
+          message: 'You must choose an option',
+          header: 'Required!',
+          buttons: ['ALRIGHT!'],
+      }).then(alertElement => {
+          alertElement.present();
+      })
+      document.form.choice.focus();
+      return false;
+  }
+      return true;
+}
+const displayAlert = document.querySelector('ion-alert-controller');
+
+//NEW SCRIPTS 
+$('#myCarousel').on('slid.bs.carousel', function (e) {
+    $('#myCarousel').carousel('2')
+  })
+  
+  $('#myCarousel').carousel('1')
+  $('#myCarousel').carousel('2') 
